702b19551f10879abfe62e754eb35a1ea88c62c1
Jorge <jorgeewillian00@hotmail.com>
2020-01-28 23:23:01 -0300

    creating class and implementing interface

---
+++ Project1/data/MySqlConnection.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Project1.data
+{
+    public class MySqlConnection : Connection
+    {
+        public void connect(){
+            
+            Console.WriteLine("connect to mysql");
+        }
+    }
+}
---
+++ Project1/data/SqlServerConnnection.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Project1.data
+{
+    public class SqlServerConnnection : Connection
+    {
+        public void connect(){
+            
+            Console.WriteLine("connect to sqlserver");
+        }
+    }
+}
