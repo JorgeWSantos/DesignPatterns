dcb34fe5e57379f3b6f7dc35f648b483a236d2c5
Jorge <jorgeewillian00@hotmail.com>
2020-01-28 23:26:04 -0300

    testing the flexibility of code on change the sgbd connection

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Project1;
+using Project1.data;
 
 namespace DesignPatterns
 {
@@ -6,7 +8,20 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MySqlConnection mysql = new MySqlConnection();
+            SqlServerConnnection sqlServer = new SqlServerConnnection();
+
+            var sgbd = mysql;
+            // var sgbd = sqlServer;
+
+            ReservationReport reservationReport = new ReservationReport(sgbd);
+            reservationReport.reportGeneration();
+
+            ReservationService reservationService = new ReservationService(sgbd);
+            reservationService.createReservation();
+
+            RoomService roomService = new RoomService(sgbd);
+            roomService.roomVerification();
         }
     }
 }
