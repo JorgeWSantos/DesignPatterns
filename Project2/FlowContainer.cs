232cd7725b6b43a632f5b6095145c9dd00441a36
Jorge <jorgeewillian00@hotmail.com>
2020-01-29 23:56:02 -0300

    Create class and implementing abstract class Container.css

---
+++ Project2/FlowContainer.cs
@@ -0,0 +1,21 @@
+
+namespace DesignPatterns.Project2
+{
+    public class FlowContainer : Container
+    {
+        public override void addComponent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void doLayout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void removeComponent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Project2/GridContainer.cs
@@ -0,0 +1,20 @@
+namespace DesignPatterns.Project2
+{
+    public class GridContainer : Container
+    {
+        public override void addComponent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void doLayout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void removeComponent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
