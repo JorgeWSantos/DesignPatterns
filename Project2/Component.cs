aca894a140b5473e9a12495190b2bfd37892d868
Jorge <jorgeewillian00@hotmail.com>
2020-01-29 22:57:03 -0300

    Create component class

---
+++ Project2/Component.cs
@@ -0,0 +1,12 @@
+namespace Project2
+{
+    public class Component
+    {
+        public string name { get; set; }
+
+        public Component(string name)
+        {
+            this.name = name;
+        }
+    }
+}
