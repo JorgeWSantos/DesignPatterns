229daf61b888430f064f5f47b4d343cbe8241f45
Jorge <jorgeewillian00@hotmail.com>
2020-01-29 23:49:29 -0300

    create class container

---
+++ Project2/Container.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace DesignPatterns.Project2
+{
+    public class Container
+    {
+        public void addComponent(){
+            
+        }
+
+        public void removeComponent(){
+            
+        }
+
+        public void doLayout(){
+            
+        }
+
+        public void dispose(){
+            
+            Console.WriteLine("dispose container");
+        }
+    }
+}
