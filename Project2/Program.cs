42244467f658c6b2a5fab589aa3cfdb83e3d2ca6
Jorge <jorgeewillian00@hotmail.com>
2020-01-31 00:17:24 -0300

    using implmentations

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -6,25 +6,39 @@ namespace DesignPatterns.Project2
     {
         static void Main(string[] args)
         {
+            //components
             Component checkBox = new Component("CheckBox");
             Component radioButton = new Component("radioButton");
             Component comboBox = new Component("comboBox");
             Component textBox = new Component("textBox");
 
+            //borders
+            // BorderSolid borderSolid = new BorderSolid();
+
             FlowContainer flowContainer = new FlowContainer();
             flowContainer.addComponent(checkBox);
             flowContainer.addComponent(radioButton);
             flowContainer.addComponent(textBox);
-            flowContainer.removeComponent(checkBox);
-            flowContainer.doLayout();
+            Border borderSolid = new BorderSolid(flowContainer);
+            borderSolid.generateBorder();
+
+            FlowContainer flowContainer2 = new FlowContainer();
+            flowContainer2.addComponent(checkBox);
+            flowContainer2.addComponent(radioButton);
+            flowContainer2.addComponent(textBox);
+            Border borderEtched = new EtchedBorder(flowContainer2);
+            borderEtched.generateBorder();
+            
 
             GridContainer gridContainer = new GridContainer(2,2);
             gridContainer.addComponent(checkBox);
             gridContainer.addComponent(textBox);
             gridContainer.addComponent(comboBox);
             gridContainer.addComponent(radioButton);
-            gridContainer.removeComponent(textBox);
-            gridContainer.doLayout();
+            // gridContainer.removeComponent(textBox);
+            borderEtched.setContainer(gridContainer);
+            borderEtched.generateBorder();
+            
         }
     }
 }
