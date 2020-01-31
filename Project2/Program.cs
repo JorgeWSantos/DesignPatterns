f87a988d006fd1e23b8f5968ae3bdf198b7ad292
Jorge <jorgeewillian00@hotmail.com>
2020-01-31 00:12:09 -0300

    implementing logic

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -6,7 +6,25 @@ namespace DesignPatterns.Project2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Component checkBox = new Component("CheckBox");
+            Component radioButton = new Component("radioButton");
+            Component comboBox = new Component("comboBox");
+            Component textBox = new Component("textBox");
+
+            FlowContainer flowContainer = new FlowContainer();
+            flowContainer.addComponent(checkBox);
+            flowContainer.addComponent(radioButton);
+            flowContainer.addComponent(textBox);
+            flowContainer.removeComponent(checkBox);
+            flowContainer.doLayout();
+
+            GridContainer gridContainer = new GridContainer(2,2);
+            gridContainer.addComponent(checkBox);
+            gridContainer.addComponent(textBox);
+            gridContainer.addComponent(comboBox);
+            gridContainer.addComponent(radioButton);
+            gridContainer.removeComponent(textBox);
+            gridContainer.doLayout();
         }
     }
 }
