1e45c80116b40c9b9c8d4ca3f97f26e347ca3c6a
Jorge <jorgeewillian00@hotmail.com>
2020-01-30 22:10:50 -0300

    adding properties to methods addComponent and remove component

--- Project2/Container.cs
+++ Project2/Container.cs
@@ -4,10 +4,9 @@ namespace DesignPatterns.Project2
 {
     public abstract class Container
     {
-        public abstract void addComponent();
-        public abstract void removeComponent();
+        public abstract void addComponent(Component component);
+        public abstract void removeComponent(Component component);
         public abstract void doLayout();
-        
         public void dispose(){
 
             Console.WriteLine("dispose container");
