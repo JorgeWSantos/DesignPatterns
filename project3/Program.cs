72e7d7c5a534ad4333db879fbcdd13c8f5516db5
Jorge <jorgeewillian00@hotmail.com>
2020-02-04 00:15:37 -0300

    using implementation

--- project3/Program.cs
+++ project3/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace DesignPatterns
+namespace DesignPatterns.Project3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TenisPlayer tenisP = new TenisPlayer("mikel", new RunFast());
+            tenisP.compete();
+            tenisP.performRunning();
+            tenisP.train();
+            tenisP.defineTatics();
+
+            Console.WriteLine("---");
+            SoccerPlayer soccerP = new SoccerPlayer("ronaldin", new RunFast());
+            soccerP.compete();
+            soccerP.performRunning();
+            soccerP.train();
+            soccerP.defineTatics();
+
+            Console.WriteLine("---");
+            ChessPlayer chessP = new ChessPlayer("gurian", new RunNoWay());
+            chessP.compete();
+            chessP.performRunning();
+            chessP.train();
+            chessP.defineTatics();
         }
     }
 }
