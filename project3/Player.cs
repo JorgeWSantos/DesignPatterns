0fb8b08805083852f27976b36a72d22932253a11
Jorge <jorgeewillian00@hotmail.com>
2020-02-04 00:14:59 -0300

    create screen Player and implementing runnable on constructor

---
+++ project3/Player.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace DesignPatterns.Project3
+{
+    public abstract class Player
+    {
+        private string name;
+        private Runnable runnable;
+
+        public Player(string name, Runnable runnable){
+
+            this.name = name;
+            this.runnable = runnable;
+        }
+
+        public void train(){
+
+            Console.WriteLine(name + " trains hard.");
+        }
+
+        public void compete(){
+
+            Console.WriteLine(name + " is very competitive.");
+        }
+
+        public void performRunning(){
+
+            runnable.doRunning();
+        }
+
+        protected string getName(){
+
+            return name;
+        }
+        
+        public abstract void defineTatics();
+    }
+}
