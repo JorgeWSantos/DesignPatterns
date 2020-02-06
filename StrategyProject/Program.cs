b68b1c38689b6bcb1b4631876c01f206d2585591
Jorge <jorgeewillian00@hotmail.com>
2020-02-06 01:30:08 -0300

    using implementation

--- StrategyProject/Program.cs
+++ StrategyProject/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using DesignPatterns.StrategyProject.Model;
+using StrategyProject.Model.Tanks.Movement;
+using StrategyProject.Model.Tanks.PrimaryWeapon;
+using StrategyProject.Model.Tanks.SecondaryWeapon;
 
-namespace StrategyProject
+namespace DesignPatterns.StrategyProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SmallTank smTank = new SmallTank(new MovementFast(), 
+                                             new PrimaryWeaponCanon(),
+                                             new SecondaryWeaponRocket());
+            smTank.movement();
+            smTank.primaryWeapon();
+            smTank.secondaryWeapon();
+            smTank.showTank();
+
+            System.Console.WriteLine("==============");
+            BigTank bgTank = new BigTank(new MovementLower(), 
+                                         new PrimaryWeaponBow(),
+                                         new SecondaryWeaponFlamethower());
+            bgTank.movement();
+            bgTank.primaryWeapon();
+            bgTank.secondaryWeapon();
+            bgTank.showTank();
+
+
+            System.Console.WriteLine("==============");
+            MediumTank mdTank = new MediumTank(new MovementLower(), 
+                                                new PrimaryWeaponBow(),
+                                                new SecondaryWeaponNoWeapon());
+            mdTank.movement();
+            mdTank.primaryWeapon();
+            mdTank.secondaryWeapon();
+            mdTank.showTank();
         }
     }
 }
