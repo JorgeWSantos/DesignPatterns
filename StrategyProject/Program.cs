8c7a5fd409df5812d6d96c53e373ce9fc23bb294
Jorge <jorgeewillian00@hotmail.com>
2020-02-05 23:57:25 -0300

    Create Strategy Project, it will implement directly the strategy pattern.

---
+++ StrategyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StrategyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
