dd78ed57513176c5fa8784351e681219b255b6c3
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-22 12:33:55 -0300

    usando o padrão na classe client.

--- Factory-Method/Program.cs
+++ Factory-Method/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Factory_Method
 {
+    //client
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Creator> auts = new List<Creator>();
+
+            Creator autFacebook = new FacebookAutCreator();
+            Creator autGoogle = new GoogleAutCreator();
+            auts.Add(autFacebook);
+            auts.Add(autGoogle);
+            foreach (var autentication in auts)
+            {
+                Autentication instance = autentication.createInstance();
+                instance.autenticar();
+            }
         }
     }
 }
