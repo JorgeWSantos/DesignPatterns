5efc85bfc863108981df93bd423eda3b8ef785f7
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-22 17:46:53 -0300

    usando o pattern prototype;

--- Prototype2/Program.cs
+++ Prototype2/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Prototype2
 {
+    //client
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Criando a primeira instância como Car
+            Car car1 = new Car("ES3", "BMW", "2020");
+
+            Car car2 = car1.clone();
+            Car car3 = car1.copy();
+            car2.brand = "Honda";
+            car3.brand = "Hyundai";
+            Console.WriteLine($"car1: {car1.model} | {car1.brand} | {car1.year}");
+            Console.WriteLine($"car2: {car2.model} | {car2.brand} | {car2.year}");
+            Console.WriteLine($"car3: {car3.model} | {car3.brand} | {car3.year}");
         }
     }
 }
