576ef3678bed8c1ea1d681e84cbb83fa2a4fa064
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-22 02:14:12 -0300

    criação da classe email.

---
+++ Builder/Email.cs
@@ -0,0 +1,11 @@
+namespace Builder
+{
+    //product
+    public class Email
+    {
+        public string to { get; set; }
+        public string from { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
+    }
+}
