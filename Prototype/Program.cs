d25c379deabe63d8a1556474fff28972817f6866
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-22 14:26:42 -0300

    usando prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prototype
+{
+    //client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro(1, "livro", "50");
+            Livro cloneLivro = (Livro)livro.clone();
+
+            Console.WriteLine($"clonado {cloneLivro.descricao}");
+        }
+    }
+}
