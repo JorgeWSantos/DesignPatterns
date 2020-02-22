019fa4936eabfee5b0d73d47084634a4cbf68fca
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-22 04:41:43 -0300

    criação do product

---
+++ Builder/Toy.cs
@@ -0,0 +1,12 @@
+namespace Builder
+{
+    //product
+    public class Toy
+    {
+        public string model { get; set; }
+        public string head { get; set; }
+        public string limbs { get; set; }
+        public string body { get; set; }
+        public string legs { get; set; }
+    }
+}
