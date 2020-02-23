de2ec8f5b74757072cd2b4fdb1145eab7e26cebd
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-23 00:04:59 -0300

    Usando o singleton através do factory method e sem o factory method.

---
+++ Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        //client
+        static void Main(string[] args)
+        {
+            //com factory method
+            Console.WriteLine("\n---with factory---");
+            CreatorDb creator = new OracleCreator();
+            IConnectionDb oracleCon = creator.createInstance();
+            oracleCon.open();
+
+            CreatorDb createSQlServer = new SqlServerCreator();
+            IConnectionDb sqlServerCon = createSQlServer.createInstance();
+            sqlServerCon.open();
+
+            //sem o factory method
+            Console.WriteLine("\n---without factory---");
+            var con = OracleConnnection.getInstance();
+            con.open();
+
+            var con2 = SqlServerConnection.getInstance();
+            con2.open();
+
+        }
+    }
+}
