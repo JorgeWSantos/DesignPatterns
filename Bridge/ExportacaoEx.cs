d88d13c440c20cc5c36de04c167c2160dbf2de83
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-26 05:01:57 -0300

    Criação da classe refinedabstraction.

---
+++ Bridge/ExportacaoEx.cs
@@ -0,0 +1,10 @@
+namespace bridge
+{
+    //refinedAbstraction
+    public class ExportacaoEx : Exportacao
+    {
+        public override void Exportar(){
+            base.Exportar();
+        }
+    }
+}
