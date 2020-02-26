b838cc7a0de7e1141e27a5e0f7c67d6a95692f7c
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-26 04:20:04 -0300

    Criação do projeto Adapter.

---
+++ Adapter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target target = new Target();
+            target.request();
+            // Target target_adaptee = new Adaptee(); não são compativeis
+            Target adptado = new Adapter();//tornando-os compativeis
+            adptado.request();
+        }
+    }
+}
