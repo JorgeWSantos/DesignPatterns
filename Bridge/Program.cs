01a3bc5900ebf283d19135b4517319777889fd85
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-26 05:00:30 -0300

    Criação do padrão bridge.

---
+++ Bridge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace bridge
+{
+    //client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exportacao exp = new ExportacaoEx();
+            //injetando objeto de implementação
+            exp.setImplementor = new ExportacaoDOC();
+            exp.Exportar();
+        }
+    }
+}
