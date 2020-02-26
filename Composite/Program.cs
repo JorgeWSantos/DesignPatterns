448a6f07bf006c3ded2e82c30787fce12e11a118
Jorge W. Santos <jorgeewillian00@hotmail.com>
2020-02-26 05:14:47 -0300

    Criação do projeto composite.

---
+++ Composite/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form("form");
+            form.Add(new Button("incluir"));
+            form.Add(new Button("excluir"));
+            form.Add(new TextBox("nome"));
+            form.Add(new TextBox("celular"));
+            form.Display();
+        }
+    }
+}
