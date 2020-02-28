51959424ab6c3a277187b92d8e3b1cb2ef6e50d3
Jorge <jorgeewillian00@hotmail.com>
2020-02-28 01:42:25 -0300

    Criação da classe abstract product.

---
+++ Abstract-Factory2/Product.cs
@@ -0,0 +1,9 @@
+namespace AbstractFactory2
+{
+    //abstract product
+    public abstract class Product
+    {
+        public abstract void AddProduct();
+        public abstract void RemoveProduct();
+    }
+}
