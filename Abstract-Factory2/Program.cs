1f2ce9526e3a5be9da31d35a833a23bda6fb389f
Jorge <jorgeewillian00@hotmail.com>
2020-02-28 01:42:05 -0300

    Criação da classe client.

---
+++ Abstract-Factory2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractFactory2
+{
+    //client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AbstractFactory absFactory = new FactoryProductB();
+            AbstractFactory absFactory = new FactoryProductA();
+            Product product = absFactory.createProduct();
+            product.AddProduct();
+            product.RemoveProduct();
+        }
+    }
+}
